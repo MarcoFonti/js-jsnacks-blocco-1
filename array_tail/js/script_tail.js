6fbd894bfe7799310c1c8c26cb99afeafd03f39c
MarcoFonti <theemporiumg@gmail.com>
2023-12-29 14:59:40 +0100

    add ex "La coda dell'array"

--- array_tail/js/script_tail.js
+++ array_tail/js/script_tail.js
@@ -1,2 +1,29 @@
 // Check 
 console.log('JS OK TAIL');
+
+//Prendo Elementi
+const userNumberElement = document.getElementById('userNumber');
+const userButtonElement = document.getElementById('userButton');
+console.log(userNumberElement, userButtonElement);
+
+// Creo Array
+const number = [];
+
+// creo Evento
+userButtonElement.addEventListener('click', function(){
+    // Prendo la risposta dell'utente 
+    const valueUser = userNumberElement.value;
+    //console.log(valueUser);
+
+    // Creo un ciclo for
+    for(let i = 1; i <= valueUser; i++){
+        // Random
+        const randomUser = Math.floor(Math.random() * 100) + 1
+        // Mettiamo i numeri casuali dentro Array
+        number.push(randomUser);
+    }
+})
+
+console.log(number);
+
+
--- textPassages.md
+++ textPassages.md
@@ -15,5 +15,21 @@
     - ALTRIMENTI: se non ce stampo: "Oh no, devo uscire a comprare il cocomero!"
 
 
+## LA CODA DELL'ARRAY
 
+1 Creo Domanda, Input, bottone e Paragrafo
+
+2 Prendi elementi (Id) e metto nel js
+
+3 Creo Array Vuoto
+
+4 Creo un Evento al click
+
+5 Prendo il value dell'utente
+
+6 Creo un ciclo for con il numero che fornisce l'utente
+
+7 Creo numeri random 
+
+8 Aggiungo numeri random all'Array
 
